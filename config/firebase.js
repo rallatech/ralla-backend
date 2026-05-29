97e1230f697189d38c144832e930fffc7dca74cf
rallatech <rallatech.dev@gmail.com>
2026-05-29 10:49:31 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -16,7 +16,10 @@ const initializeFirebase = () => {
       type: "service_account",
       project_id: (process.env.FIREBASE_PROJECT_ID || '').trim(),
       private_key_id: (process.env.FIREBASE_PRIVATE_KEY_ID || '').trim(),
-      private_key: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n').trim(),
+      privateKey: Buffer.from(
+                    process.env.FIREBASE_PRIVATE_KEY_BASE64,
+                   'base64'
+                  ).toString('utf8'),
       client_email: (process.env.FIREBASE_CLIENT_EMAIL || '').trim(),
       client_id: (process.env.FIREBASE_CLIENT_ID || '').trim(),
       auth_uri: (process.env.FIREBASE_AUTH_URI || '').trim(),
